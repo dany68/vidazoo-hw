9afab5372017a89adb89c75a2b5bf439c5988436
Daniel <delmalem697@gmail.com>
2023-10-30 00:07:12 +0200

    Update parser to Validate domains / Error Count / Time

--- server/index.ts
+++ server/index.ts
@@ -5,7 +5,8 @@ const app = express()
 
 app.get('/api/test', async (req, res) => {
     try {
-        const fileUrl = 'https://www.msn.com/ads.txt'; // Replace with the URL of the file you want to fetch
+        // const fileUrl = 'https://www.bbc.com/ads.txt'; // Replace with the URL of the file you want to fetch
+        const fileUrl = 'https://msn.com/ads.txt'; // Replace with the URL of the file you want to fetch
 
         const response = await fetch(fileUrl);
 
@@ -16,7 +17,9 @@ app.get('/api/test', async (req, res) => {
         const text = await response.text();
         const result = await parseAdsTxt(text)
 
-        res.send(result);
+        console.log(result)
+
+        res.json(result);
     } catch (error) {
         console.error('An error occured with the fetch operation:', error);
         res.status(500).send('Error fetching the file');
--- utils/parser.ts
+++ utils/parser.ts
@@ -1,6 +1,7 @@
 const EMPTY_LINE = /^\s*$/;
 const NEW_LINE = /\r\n|\n|\r/;
 const COMMENT_LINE = /^\s*#/;
+const VALID_DOMAIN = /^(?:(?!-)[A-Za-z0-9-]{1,63}(?<!-)\.)+[A-Za-z]{2,6}$/;
 
 /**
  * Parse ads.txt data
@@ -8,8 +9,10 @@ const COMMENT_LINE = /^\s*#/;
 export const parseAdsTxt = (fileText) => {
     return new Promise((resolve, reject) => {
         try {
+            const t0 = performance.now();
             const lines = fileText.split(NEW_LINE);
             const advertiserDomains = {};
+            let parseErrors = 0;
 
             lines.forEach(line => {
                 if (COMMENT_LINE.test(line) || EMPTY_LINE.test(line)) {
@@ -18,10 +21,21 @@ export const parseAdsTxt = (fileText) => {
 
                 const domain = line.split(',')[0].trim();
 
+                if (! VALID_DOMAIN.test(domain)) {
+                    parseErrors++;
+                    return;
+                }
+
                 advertiserDomains[domain] = (advertiserDomains[domain] || 0) + 1;
             });
 
-            resolve(advertiserDomains);
+            const t1 = performance.now();
+
+            resolve({
+                advertiserDomains,
+                time: (t1 - t0).toFixed(2),
+                parseErrors,
+            });
         } catch (err) {
             console.error('Error reading the file:', err);
             reject({});
